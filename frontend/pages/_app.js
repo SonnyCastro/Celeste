f3b6de18fa2cd868480951ec0ea065391101d383
SonnyCastro <sonnycastro0606@gmail.com>
2021-03-24 13:05:06 -0400

    name change

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,6 @@
 // /* _app.js */
 
 // import App from "next/app";
-// import Head from "next/head";
-// import Layout from "../components/Layout";
 import withApollo from "../lib/apollo";
 import React, { Component } from 'react'
 
