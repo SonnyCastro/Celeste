aa7ec78b9ebcda48950ce5681cd4c568f81d99af
SonnyCastro <sonnycastro0606@gmail.com>
2021-07-21 18:10:52 -0400

    push new edits for individual asset page

--- pages/categories/asset/[name].js
+++ pages/categories/asset/[name].js
@@ -1,9 +1,32 @@
+import styles from "../../../styles/individualAsset.module.css";
 const individualAsset = ({ asset }) => {
   return (
-    <div style={{ padding: "9rem 5.5rem", color: "white" }}>
-      <h1>Individual asset page</h1>
-      <p>What: {asset.What}</p>
-      <p style={{ margin: "1rem 0" }}>WHY: {asset.Why}</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{asset.name} Overview</h1>
+      <hr
+        style={{ color: "white", border: "1px solid white", width: "100%" }}
+      />
+      <p className={styles.what + " " + styles.why}>What is {asset.name}?</p>
+      <p className={styles.description}>{asset.What}</p>
+      <p className={styles.what + " " + styles.why}>Why use {asset.name}?</p>
+      <p className={styles.description}>{asset.Why}</p>
+      <p className={styles.what + " " + styles.totalValueLocked}>
+        Total Value Locked
+      </p>
+      <div className={styles.info}>
+        <p className={styles.infoTextSmall}>-16.8%</p>
+        <p className={styles.infoText}>$1.79B</p>
+      </div>
+      <p className={styles.what + " " + styles.tokenPrice}>Token Price</p>
+      <div className={styles.info}>
+        <p className={styles.infoText}>$103.62</p>
+      </div>
+      <p className={styles.what + " " + styles.followTwitterAccounts}>Follow</p>
+      <div className={styles.twitterAccounts}>
+        <div className={styles.twitterAccount}>@StaniKulechov</div>
+        <div className={styles.twitterAccount}>@LemiScate</div>
+      </div>
+      <p className={styles.title}>Browse More</p>
     </div>
   );
 };
